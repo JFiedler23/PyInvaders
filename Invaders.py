b9b41571f1cfe8233a8d5ac03f9bee2e328fd6d2
JFiedler23 <43707498+JFiedler23@users.noreply.github.com>
2020-05-25 14:23:06 -0500

    Laser shooting
    Added laser shooting functionality.

--- Invaders.py
+++ Invaders.py
@@ -12,6 +12,22 @@ ssImage = pygame.image.load("Images/spaceship.png")
 pygame.display.set_icon(ssImage)
 ssImage = ssImage.convert()
 
+#loading laser image
+laserImg = pygame.image.load("Images/laser_bullet.png")
+laserImg = laserImg.convert()
+
+class Laser(object):
+    def __init__(self, width, height, x, y):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.velocity = 5
+
+    def draw(self, screen):
+        screen.blit(laserImg, (self.x, self.y))
+
+
 class Player(object):
     def __init__(self, width, height, x, y):
         self.width = width
@@ -21,29 +37,32 @@ class Player(object):
         self.rightOffset = 5
         self.velocity = 5
 
-    def controller(self, screenWidth):
-        #Getting all key presses
-        keys = pygame.key.get_pressed()
+    def draw(self, screen):
+        screen.blit(ssImage, (self.x, self.y))
 
-        #checking for player input
-        if keys[pygame.K_RIGHT] and self.x < (screenWidth - self.velocity - self.width + self.rightOffset):
-            self.x += self.velocity
-        if keys[pygame.K_LEFT] and spaceship.x > 0:
-            self.x -= self.velocity
-        if keys[pygame.K_SPACE]:
-            print("Fire!")
 
 #framerate clock
 clock = pygame.time.Clock()
 
+#initializing Player
+spaceship = Player(64, 64, int(screenWidth / 2), int(screenHeight - 64))
+
 #image draw
 def redrawGameWindow():
     screen.fill((0,0,0))
-    screen.blit(ssImage, (spaceship.x,spaceship.y))
+    spaceship.draw(screen)
+
+    for laser in lasers:
+        laser.draw(screen)
+
     pygame.display.update()
 
-#initializing Player
-spaceship = Player(64, 64, int(screenWidth / 2), int(screenHeight - 64))
+lasers = []
+
+#creating reload event
+RELOAD_SPEED = 450
+reloaded_event = pygame.USEREVENT + 1
+reloaded = True
 
 #main loop
 run = True
@@ -51,11 +70,34 @@ while run:
     clock.tick(60)
 
     #Checking if game window has been closed
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    if pygame.event.get(pygame.QUIT): run = False
 
-    spaceship.controller(screenWidth)
+    #Checking if reload event has occured
+    for event in pygame.event.get():
+        if event.type == reloaded_event:
+            reloaded = True
+            pygame.time.set_timer(reloaded_event, 0)
+
+    #laser manager
+    for laser in lasers:
+        if laser.y > 0 and laser.y < 480:
+            laser.y -= laser.velocity
+        else:
+            lasers.pop(lasers.index(laser))
+
+    #Getting all key presses
+    keys = pygame.key.get_pressed()
+
+    #checking for player input
+    if keys[pygame.K_RIGHT] and spaceship.x < (screenWidth -spaceship.velocity - spaceship.width + spaceship.rightOffset):
+        spaceship.x += spaceship.velocity
+    if keys[pygame.K_LEFT] and spaceship.x > 0:
+        spaceship.x -= spaceship.velocity
+    if keys[pygame.K_SPACE]:
+        if reloaded:
+            lasers.append(Laser(32, 32, int(spaceship.x + spaceship.width // 4), int(spaceship.y + spaceship.height // 6)))
+            reloaded = False
+            pygame.time.set_timer(reloaded_event, RELOAD_SPEED)
 
     redrawGameWindow()
 
